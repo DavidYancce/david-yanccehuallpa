f41284f96da268216cda345c47361bddac8ddc61
David Yanccehuallpa <david.yanccehuallpa.q@uni.pe>
2024-08-21 06:23:35 -0500

    feat: seed estados de pedidos

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from '@prisma/client';
+import { PedidosStates } from '../src/enums/pedidos-states.enum';
 import { UserRoles } from '../src/enums/user-roles.enum';
 const prisma = new PrismaClient();
 
@@ -7,6 +8,13 @@ const roles = [
   [UserRoles.VENDEDOR, 'Vendedor'],
   [UserRoles.DELIVERY, 'Delivery'],
   [UserRoles.REPARTIDOR, 'Repartidor'],
+];
+
+const estadosPedidos = [
+  [PedidosStates.POR_ATENDER, 'Por atender'],
+  [PedidosStates.EN_PROCESO, 'En proceso'],
+  [PedidosStates.DELIVERY, 'En delivery'],
+  [PedidosStates.RECIBIDO, 'Recibido'],
 ];
 
 const seedRoles = async () => {
@@ -26,8 +34,26 @@ const seedRoles = async () => {
   await prisma.$executeRawUnsafe(raw);
 };
 
+const seedEstadosPedidos = async () => {
+  const raw = [
+    'SET IDENTITY_INSERT estados ON',
+    ...estadosPedidos.map(
+      ([id, nombre]) => `
+        IF NOT EXISTS (SELECT 1 FROM estados WHERE id = ${id})
+        BEGIN
+          INSERT INTO estados (id, nombre)
+          VALUES (${id}, N'${nombre}')
+        END`,
+    ),
+    'SET IDENTITY_INSERT estados OFF',
+  ].join('\n');
+
+  await prisma.$executeRawUnsafe(raw);
+};
+
 seedRoles()
   .then(async () => {
+    await seedEstadosPedidos();
     await prisma.$disconnect();
   })
   .catch(async (e) => {
---
+++ src/enums/pedidos-states.enum.ts
@@ -0,0 +1,6 @@
+export enum PedidosStates {
+  POR_ATENDER = 1,
+  EN_PROCESO = 2,
+  DELIVERY = 3,
+  RECIBIDO = 4,
+}
